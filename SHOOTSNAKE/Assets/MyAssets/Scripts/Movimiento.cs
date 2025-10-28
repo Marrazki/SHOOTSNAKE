01a2f8ae277e4654151ad5e6c3a33f7dd6de006f
Urko Artzeniega <urko_artzeniega@euneiz.com>
2025-10-28 01:17:14 +0100

    Nuevos controles de movimiento
    Ahora te puedes mover con las flechas direccionales ademas de WASD y se ha cambiado el tamaño del player

--- SHOOTSNAKE/Assets/MyAssets/Scripts/Movimiento.cs
+++ SHOOTSNAKE/Assets/MyAssets/Scripts/Movimiento.cs
@@ -1,45 +1,30 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-
     public float velocidad = 5f;
-    public int direccion = 3;
+    public int direccion = 3; // 0=arriba, 1=izq, 2=abajo, 3=der
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
+        // Cambiar dirección solo cuando se pulsa una tecla (no hace diagonales)
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             direccion = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             direccion = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
             direccion = 2;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             direccion = 3;
-        }
+
+        // Mover continuamente en la última dirección elegida
         if (direccion == 0)
-        {
             transform.Translate(Vector2.up * velocidad * Time.deltaTime);
-        }
-        if (direccion == 1)
-        {
+        else if (direccion == 1)
             transform.Translate(Vector2.left * velocidad * Time.deltaTime);
-        }
-        if (direccion == 2)
-        {
+        else if (direccion == 2)
             transform.Translate(Vector2.down * velocidad * Time.deltaTime);
-        }
-        if (direccion == 3)
-        {
+        else // 3
             transform.Translate(Vector2.right * velocidad * Time.deltaTime);
-        }
     }
 }
